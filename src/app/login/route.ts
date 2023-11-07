93e8a0dd32e2f4ea53d72789ff4f215f02add33a
Akinori Ito <voice0726@gmail.com>
2023-11-07 17:33:58 +0900

    fix: return NextResponse.redirect when already signed-in

--- src/app/login/route.ts
+++ src/app/login/route.ts
@@ -1,14 +1,18 @@
 import { URL } from 'url';
 
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 import { signIn } from '@/auth';
+import { APP_HOST } from '@/config';
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const redirectTo = searchParams.get('redirect_to') || undefined;
+
   await signIn('google', {
     redirectTo,
     redirect: !!redirectTo,
   });
+
+  return NextResponse.redirect(redirectTo || `${APP_HOST}/dashboard`);
 }
