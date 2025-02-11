c0ce45b84284d860b8502b502e54a8613786c26b
Akinori Ito <akinori@1t0.jp>
2025-02-11 09:20:40 +0900

    ci: update github action flows to avoid unnecessary run (#61)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  '*.{ts,tsx}': ['pnpm run lint:check', "bash -c 'pnpm run types:check'", 'pnpm run prettier:check'],
+  '*.{ts,tsx,mts,cts}': ['pnpm run lint:check', "bash -c 'pnpm run types:check'", 'pnpm run prettier:check'],
   '*': ['secretlint'],
 };
