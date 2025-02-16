bd872cea989cd7d58707c39efcb5daf564be29f5
Akinori Ito <akinori@1t0.jp>
2025-02-16 18:32:05 +0900

    fix(mock): make mock run only when dev and test (#90)

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,5 +1,5 @@
 export async function register() {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
+  if (process.env.NEXT_RUNTIME === 'nodejs' && process.env.NODE_ENV !== 'production') {
     const { server } = await import('./mocks/node');
     server.listen({ onUnhandledRequest: 'bypass' });
   }
