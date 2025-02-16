a91d00dda35cbe2bc0b254c171ed7286afd44455
Akinori Ito <akinori@1t0.jp>
2025-02-16 18:51:35 +0900

    fix: exclude favicon from auth required list and fix redirect path (#93)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,14 @@
 import { NextResponse } from 'next/server';
 
 import { auth } from '~/auth';
+import { APP_HOST } from '~/config';
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|login).+)'],
+  matcher: ['/((?!api|_next/static|_next/image|login|favicon).+)'],
 };
 
 export default auth((req) => {
   if (!req.auth) {
-    return NextResponse.redirect('http://localhost:3000/login');
+    return NextResponse.redirect(`${APP_HOST}/login`);
   }
 });
